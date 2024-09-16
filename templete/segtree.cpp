2adf530a5af639feed7ec9a3cf7f8ebfae09b3e9
kss418 <jis1595@naver.com>
2024-09-16 20:22:55 +0900

    lazy prop 추가

--- templete/segtree.cpp
+++ templete/segtree.cpp
@@ -176,3 +176,63 @@ public:
 };
 
 //LAZY PROP
+class _prop {
+public:
+    vector<ll> seg, lazy, arr; ll n;
+
+    _prop(ll n) {
+        this->n = n; arr.resize(n + 1);
+        seg.resize(4 * n + 1); lazy.resize(4 * n + 1);
+    }
+
+    void con(ll idx, ll val) { arr[idx] = val; }
+
+    void init() { init(1, n); }
+    ll init(ll l, ll r, ll node = 1) {
+        if (l == r) return seg[node] = arr[l];
+        ll mid = (l + r) >> 1;
+        seg[node] = init(l, mid, node * 2) + init(mid + 1, r, node * 2 + 1);
+        return seg[node];
+    }
+
+    void propagate(ll l, ll r, ll node) {
+        if (!lazy[node]) return;
+        if (l != r) {
+            lazy[node * 2] += lazy[node];
+            lazy[node * 2 + 1] += lazy[node];
+        }
+        seg[node] += lazy[node] * (r - l + 1);
+        lazy[node] = 0;
+    }
+
+    void add(ll st, ll en, ll val) { add(st, en, val, 1, n); }
+    void add(ll st, ll en, ll val, ll l, ll r, ll node = 1) {
+        propagate(l, r, node);
+
+        if (st > r || en < l) return;
+        if (l >= st && r <= en) {
+            lazy[node] += val; propagate(l, r, node);
+            return;
+        }
+
+        ll mid = (l + r) >> 1;
+        add(st, en, val, l, mid, node * 2);
+        add(st, en, val, mid + 1, r, node * 2 + 1);
+
+        seg[node] = seg[node * 2] + seg[node * 2 + 1];
+    }
+
+    ll query(ll st, ll en) { return query(st, en, 1, n); }
+    ll query(ll st, ll en, ll l, ll r, ll node = 1) {
+        propagate(l, r, node);
+
+        if (st > r || en < l) return 0;
+        if (l >= st && r <= en) return seg[node];
+
+        ll mid = (l + r) >> 1;
+        ll ret = query(st, en, l, mid, node * 2);
+        ret += query(st, en, mid + 1, r, node * 2 + 1);
+
+        return ret;
+    }
+};
