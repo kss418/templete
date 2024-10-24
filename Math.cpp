b0537452342029cc03ca4b658c9193fc0c2a484e
kss418 <jis1595@naver.com>
2024-10-24 14:31:06 +0900

    에라토스체 수정

--- Math.cpp
+++ Math.cpp
@@ -1,33 +1,60 @@
 #include <bits/stdc++.h>
 #define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
 using namespace std; typedef long long ll;
+using ld = long double;
 using ull = unsigned long long;
 using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
 ll n, m, k, t; string s;
-constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
 
 //에라토스체
-class _es {
+class _es { // flag (1 == prime / 2 == phi / 4 == mobius)
 public:
-    ll n; vector <ll> p, pn;
-    _es(ll n) {
+    ll n; vector <ll> pv, pn, mov, phiv;
+    vector <bool> v;
+    _es(ll n, ll flag = 1) {
         this->n = n;
-        p.resize(n + 1, -1);
-    }
 
-    void init() {
-        p[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            if (p[i] != -1) continue; p[i] = i;
-            pn.push_back(i);
-            for (int j = 2 * i; j <= n; j += i) {
-                if (p[j] == -1) p[j] = i;
+        if(flag & 1){
+            pv.resize(n + 1, -1); pv[1] = 1;
+            for (ll i = 2;i <= n;i++) {
+                if(pv[i] != -1) continue; pv[i] = i;
+                pn.push_back(i);
+                for (int j = 2 * i; j <= n; j += i) {
+                    if(pv[j] == -1) pv[j] = i;
+                }
+            }
+        }
+
+        if(flag & 2){
+            phiv.resize(n + 1); iota(all(phiv), 0);
+            v.resize(n + 1);
+            for(ll i = 2;i <= n;i++){
+                if(v[i]) continue;
+                for(ll j = i;j <= n;j += i){
+                    phiv[j] /= i; phiv[j] *= (i - 1); v[j] = 1;
+                }
+            }
+        }
+
+        if(flag & 4){
+            mov.resize(n + 1, -2);
+            mov[0] = 0; mov[1] = 1;
+            for(ll i = 2;i <= n;i++){
+                if(mov[i] != -2) continue; mov[i] = -1;
+                for(ll j = 2 * i;j <= n;j += i) {
+                    if(mov[j] == -2) mov[j] = 1;
+                    mov[j] *= -1;
+                }
+                for(ll j = i * i;j <= n; j += i * i) mov[j] = 0;
             }
         }
     }
 
     ll mp(ll n) { // 최소 소인수 반환
-        return p[n];
+        return pv[n];
     }
 
     vector<ll> ret() { // 소수 벡터 반환
@@ -36,7 +63,15 @@ public:
 
     bool prime(ll n) { // 소수 판별
         if (n == 1) return 0;
-        return p[n] == n;
+        return pv[n] == n;
+    }
+
+    ll mobius(ll n){
+        return mov[n];
+    }
+
+    ll phi(ll n){
+        return phiv[n];
     }
 };
 
