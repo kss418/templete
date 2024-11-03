bafeab5a4b6fd04694809a35d340f72257486430
kss418 <jis1595@naver.com>
2024-11-03 15:03:30 +0900

    선분 교차 판정 추가

--- Geometry.cpp
+++ Geometry.cpp
@@ -19,11 +19,39 @@ public:
         return ret({x1, y1}, {x2, y2}, {x3, y3});
     }
     ll ret(pll v1, pll v2, pll v3){
-        ll num = v1.x * v2.y + v2.x * v3.y + v3.x * v1.y;
-        num -= v1.y * v2.x + v2.y * v3.x + v3.y * v1.x;
+        ll num = (v2.x - v1.x) * (v3.y - v1.y);
+        num -= (v3.x - v1.x) * (v2.y - v1.y);
 
         if(num > 0) return 1; // cw
         else if(num < 0) return -1; // ccw
         return 0;
+    }
+};
+
+//LINE INTERSECTION
+class _li{
+public:
+    _li() {}
+
+    ll ccw(pll v1, pll v2, pll v3){
+        ll num = (v2.x - v1.x) * (v3.y - v1.y);
+        num -= (v3.x - v1.x) * (v2.y - v1.y);
+
+        if(num > 0) return 1; // cw
+        else if(num < 0) return -1; // ccw
+        return 0;
+    }
+
+    bool ret(pll av1, pll av2, pll bv1, pll bv2){
+        ll ac = ccw(av1, av2, bv1) * ccw(av1, av2, bv2);
+        ll bc = ccw(bv1, bv2, av1) * ccw(bv1, bv2, av2);
+
+        if(!ac && !bc){
+            if(av1 > av2) swap(av1, av2);
+            if(bv1 > bv2) swap(bv1, bv2);
+            return bv1 <= av2 && av1 <= bv2;
+        }
+
+        return ac <= 0 && bc <= 0;
     }
 };
