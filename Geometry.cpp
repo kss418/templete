4cd163bdc4b2f9bce0f8219135de912d4378a935
kss418 <jis1595@naver.com>
2024-11-04 16:36:04 +0900

    convex hull 추가

--- Geometry.cpp
+++ Geometry.cpp
@@ -58,3 +58,64 @@ public:
         return ac <= 0 && bc <= 0;
     }
 };
+
+//CONVEX HULL
+class _ch{
+public:
+    _ch() {}
+    vector <ll> result;
+
+    class node{
+    public:
+        ll x, y, dx, dy;
+        node(pll a) : node(a.x, a.y, 1, 0) {};
+        node(ll x, ll y, ll dx, ll dy){
+            this->x = x; this->y = y;
+            this->dx = dx; this->dy = dy;
+        }
+
+        bool operator <(node& ot){
+            if(dy * ot.dx != dx * ot.dy) return dy * ot.dx < dx * ot.dy;
+            if(y != ot.y) return y < ot.y;
+            return x < ot.x;
+        }
+        operator pll(){ return { x, y }; }
+    };
+    vector <node> arr;
+
+    ll ccw(pll v1, pll v2, pll v3){
+        ll num = (v2.x - v1.x) * (v3.y - v1.y);
+        num -= (v3.x - v1.x) * (v2.y - v1.y);
+
+        if(num > 0) return 1; // cw
+        else if(num < 0) return -1; // ccw
+        return 0;
+    }
+    
+    void add(ll a, ll b) { add({a, b}); }
+    void add(pll a){ arr.push_back(a); }
+
+    void init(){
+        sort(all(arr));
+        for(int i = 1;i < arr.size();i++){
+            arr[i].dx = arr[i].x - arr[0].x;
+            arr[i].dy = arr[i].y - arr[0].y;
+        }
+        sort(arr.begin() + 1, arr.end());
+
+        result.push_back(0); result.push_back(1);
+        ll nxt = 2;
+
+        while(nxt < arr.size()){
+            while(result.size() >= 2){
+                ll fi = result.back(); result.pop_back();
+                ll se = result.back();
+                ll chk = ccw(arr[se], arr[fi], arr[nxt]);
+                if(chk > 0){ result.push_back(fi); break; }
+            }
+            result.push_back(nxt++);
+        }
+    }
+
+    vector <ll> ret(){ return result; }
+};
