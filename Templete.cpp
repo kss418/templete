2ae81dde3e408e59e87dc55358ae90ad89b56bc6
kss418 <jis1595@naver.com>
2024-11-19 14:01:54 +0900

    가중치 union find 추가 및 union find set 추가

--- Templete.cpp
+++ Templete.cpp
@@ -10,7 +10,7 @@ using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
 ll n, m, k, t; string s;
 constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
 
-// 유파
+// UNION FIND
 class _uf { 
 public:
     ll n; vector <ll> p, si;
@@ -33,17 +33,11 @@ public:
         p[b] = a, si[a] += si[b];
     }
 
-    ll size(ll num){
-        return si[find(num)];
-    }
-
-    ll same(ll a, ll b) {
-        if (find(a) == find(b)) return 1;
-        return 0;
-    }
+    ll size(ll num){ return si[find(num)]; }
+    bool same(ll a, ll b) { return find(a) == find(b); }
 };
 
-//이분 탐색
+//BINARY SEARCH
 template <typename T = ll>
 class _bs { // 0-based index
 public:
@@ -208,7 +202,7 @@ public:
     }
 };
 
-//삼분 탐색
+//TERNARY SEARCH
 class _ts { // 0-based index
 public:
     ll l, r;
@@ -233,4 +227,74 @@ public:
 
         return result;
     }
+};
+
+// WEIGHTED UNION FIND
+class _uf { 
+public:
+    ll n; vector <ll> p, d, dis;
+    
+    _uf(){}
+    _uf(ll n) { 
+        this->n = n; dis.resize(n + 1);
+        p.resize(n + 1, -1); d.resize(n + 1); 
+    }
+
+    pll find(ll num) { 
+        if (p[num] == -1) return { num, 0ll };
+        auto [pa, diff] = find(p[num]);
+        p[num] = pa; d[num] += diff;
+        return { p[num], d[num] };
+    }
+
+    void merge(ll a, ll b, ll c) {
+        auto [pa, da] = find(a);
+        auto [pb, db] = find(b);
+        if (pa == pb) { dis[pb] |= (db - da - c != 0); return; }
+        p[pa] = pb; dis[pb] |= dis[pa];
+        d[pa] = db - da - c; 
+    }
+
+    pll diff(ll a, ll b){
+        auto [pa, da] = find(a);
+        auto [pb, db] = find(b);
+        if(pa == pb && !dis[pa]) return { 1, db - da };
+        else if(pa == pb && dis[pa]) return { 0, -1 };
+        else return { 0, 0 };
+    }
+
+    bool same(ll a, ll b) { return find(a) == find(b); }
+};
+
+// UNION FIND SET
+class _uf { 
+public:
+    ll n; vector <ll> p, si, l, r;
+    
+    _uf(){}
+    _uf(ll n) { 
+        this->n = n;
+        p.resize(n + 1, -1); si.resize(n + 1, 1);
+        l.resize(n + 1); r.resize(n + 1);
+        iota(all(l), 0); iota(all(r), 0);
+    }
+
+    ll find(ll num) { 
+        if (p[num] == -1) return num;
+        return p[num] = find(p[num]);
+    }
+
+    void merge(ll a, ll b) {
+        a = find(a); b = find(b);
+        if (a == b) return;
+        if (size(a) < size(b)) swap(a, b);
+        p[b] = a, si[a] += si[b];
+        l[a] = min(l[a], l[b]);
+        r[a] = max(r[a], r[b]);
+    }
+
+    ll size(ll num){ return si[find(num)]; }
+    bool same(ll a, ll b) { return find(a) == find(b); }
+    ll prv(ll num){ return find(l[find(num)] - 1); }
+    ll nxt(ll num){ return find(r[find(num)] + 1); }
 };
