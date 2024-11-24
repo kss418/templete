84c6e24138fba5f9bb2041d75acc02192c39efca
kss418 <jis1595@naver.com>
2024-11-24 16:09:19 +0900

    다각형 내부 판별 추가

--- Geometry.cpp
+++ Geometry.cpp
@@ -220,3 +220,54 @@ public:
 
     ld f(ld x) { return a * x + b; }
 }; 
+
+// 점 다각형 내부 판별
+class _inter{ 
+public:
+    class _li{
+    public:
+        _li() {}
+
+        ll ccw(pll v1, pll v2, pll v3){
+            ll num = (v2.x - v1.x) * (v3.y - v1.y);
+            num -= (v3.x - v1.x) * (v2.y - v1.y);
+
+            if(num > 0) return 1; 
+            else if(num < 0) return -1; 
+            return 0;
+        }
+
+        bool ret(pair<pll, pll> a, pair<pll, pll> b) {
+            return ret(a.x, a.y, b.x, b.y);
+        }
+        bool ret(pll av1, pll av2, pll bv1, pll bv2){
+            ll ac = ccw(av1, av2, bv1) * ccw(av1, av2, bv2);
+            ll bc = ccw(bv1, bv2, av1) * ccw(bv1, bv2, av2);
+
+            if(!ac && !bc){
+                if(av1 > av2) swap(av1, av2);
+                if(bv1 > bv2) swap(bv1, bv2);
+                return bv1 <= av2 && av1 <= bv2;
+            }
+
+            return ac <= 0 && bc <= 0;
+        }
+    }; _li li;
+
+    _inter(){}
+    bool ret(vector<pll>& p, pll now){
+        ll cnt = 0, flag = 0, sz = p.size();
+        if(sz < 3) return 0;
+        pll nxt = {1e12, now.y + 1};
+
+        for(int i = 0;i < sz;i++){
+            cnt += li.ret({p[i], p[(i + 1) % sz]}, {now, nxt});
+        }
+
+        for(int i = 0;i < sz;i++){
+            flag += li.ret({p[i], p[(i + 1) % sz]}, {now, now});
+        }
+
+        return (cnt % 2 | flag);
+    }
+}; 
