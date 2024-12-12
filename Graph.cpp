81fd5a218c5983578ec2657c154f97dbd388c2c3
kss418 <jis1595@naver.com>
2024-12-12 16:17:53 +0900

    다익스트라 수정

--- Graph.cpp
+++ Graph.cpp
@@ -53,52 +53,62 @@ public:
 };
 
 // 다익
-template <typename T = ll>
 class _dij {
 public:
-    using ptl = pair <T, ll>;
-    ll n; vector <T> d;
+    class node{
+    public:
+        ll d;
+        ll num() const{ return d; }
+        bool operator<(const node& ot) const{ return num() < ot.num(); }
+        bool operator>(const node& ot) const{ return num() > ot.num(); }
+        bool operator==(const node& ot) const{ return num() == ot.num(); }
+        bool operator<=(const node& ot) const{ return num() <= ot.num(); }
+        node operator+(const node& ot) const{
+            return {d + ot.d};
+        }
+    };
+    node mx(){ return {INF}; }
+    node mn(){ return {0}; }
+
+    using ptl = pair <node, ll>;
+    ll n; vector <node> d;
     vector <ll> pre;
     vector <vector<ptl>> adj;
     priority_queue <ptl, vector<ptl>, greater<ptl>> pq;
 
     _dij(ll n) {
         this->n = n;
-        d.resize(n + 1, INF); pre.resize(n + 1, -1);
-        adj.resize(n + 1);
+        d.resize(n + 1, mx());
+        pre.resize(n + 1, -1); adj.resize(n + 1);
     }
 
-    void add(ll st, ll en, T c = 1) { // 양방향
+    void add(ll st, ll en, node c) { // 양방향
         adj[st].push_back({ c,en });
         adj[en].push_back({ c,st });
     }
-
-    void addsol(ll st, ll en, T c = 1) { // 단방향
+    void addsol(ll st, ll en, node c) { // 단방향
         adj[st].push_back({ c,en });
     }
 
-    void init(ll st, T fi = 0) {
-        d[st] = fi;
-        pq.push({ d[st], st });
+    void init(ll st) {
+        pq.push({ mn(), st });
 
         while (!pq.empty()) {
-            auto [cd, cur] = pq.top(); pq.pop();
-            if (cd > d[cur]) continue;
-
-            for (auto& nn : adj[cur]) {
-                auto [nd, nxt] = nn;
-                if (d[nxt] <= nd + cd) continue;
-
-                d[nxt] = nd + cd;
-                pre[nxt] = cur;
-                pq.push({ d[nxt], nxt });
+            auto [cn, cur] = pq.top(); pq.pop();
+            if (d[cur] <= cn) continue;
+            d[cur] = cn;
+            
+            for (auto& i : adj[cur]) {
+                auto [nn, nxt] = i;
+                node pl = nn + cn;
+        
+                if (d[nxt] <= pl) continue;
+                pre[nxt] = cur; pq.push({ pl, nxt });
             }
         }
     }
 
-    T ret(ll n) {
-        return d[n];
-    }
+    node ret(ll n) { return d[n]; }
 };
 
 // LCA
