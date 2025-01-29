051d6c4c7ec5f112db9f983dc92c775d6269f52e
kss418 <jis1595@naver.com>
2025-01-29 20:21:44 +0900

    세그트리 수정

--- Segtree.cpp
+++ Segtree.cpp
@@ -65,30 +65,25 @@ public:
     public:
         ll v;
         node() : node(0){}
-        node(ll v) {
-            this->v = v;
-        }
-
-        node operator + (node ot){ // update
-            ll now = v + ot.v;
-            return node(now);
-        }
-
-        node operator += (T ot){ // add
-            v += ot;
-            return node(v);
+        node(ll v) : v(v) {}
+        
+        node operator += (node ot){ // add
+            return node();
         }
 
         operator T(){ // update -> query
             return v;
         }
     };
-    vector <node> seg, arr;
+    vector <node> seg;
 
     _seg(){}
-    _seg(ll n) {
-        this->n = n;
-        arr.resize(n + 1); seg.resize(4 * n + 1);
+    _seg(ll n) { this->n = n; seg.resize(4 * n + 1, node()); }
+
+    node merge(node l, node r){
+        return{
+            l.v + r.v
+        };
     }
 
     T query(ll st, ll en) { return query(st, en, 0, n); }
@@ -96,17 +91,18 @@ public:
         if (en < l || st > r) return _seg::node();
         if (st <= l && en >= r) return seg[node];
         ll mid = (l + r) >> 1;
-        return query(st, en, l, mid, node * 2) + query(st, en, mid + 1, r, node * 2 + 1);
+        return merge(query(st, en, l, mid, node * 2), query(st, en, mid + 1, r, node * 2 + 1));
     }
 
     node update(ll idx, ll val) { return update(idx, val, 0, n); }
     node update(ll idx, ll val, ll l, ll r, ll node = 1) {
         if (idx < l || idx > r) return seg[node];
-        if (l == r) return seg[node] = val;
+        if (l == r) {
+            seg[node] = val;
+            return seg[node];
+        }
         ll mid = (l + r) >> 1;
-
-        seg[node] = update(idx, val, l, mid, node * 2) + update(idx, val, mid + 1, r, node * 2 + 1);
-        return seg[node];
+        return seg[node] = merge(update(idx, val, l, mid, node * 2), update(idx, val, mid + 1, r, node * 2 + 1));;
     }
 
     void add(ll idx, ll val) { add(idx, val, 0, n); }
@@ -115,10 +111,11 @@ public:
         seg[node] += val;
         if (l == r) return;
         ll mid = (l + r) >> 1;
-
         add(idx, val, l, mid, node * 2);
         add(idx, val, mid + 1, r, node * 2 + 1);
     }
+
+    void clear(){ seg.clear(); seg.assign(4 * n + 1, node()); }
 };
 
 //LAZY PROP
