7d146355cc6a1f7ab98553073a4d5718b5b4fd5b
kss418 <jis1595@naver.com>
2025-12-26 13:30:19 +0900

    DSU.cpp 추가

---
+++ DSU.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long; using u64 = uint64_t;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+
+class _uf { 
+public:
+    vector <ll> p, sz; ll n;
+    _uf(){}
+    _uf(ll n) {  
+        this->n = n;
+        p.resize(n + 1, -1); sz.resize(n + 1, 1);
+    }
+
+    ll find(ll num) { 
+        if (p[num] == -1) return num;
+        return p[num] = find(p[num]);
+    }
+
+    void merge(ll a, ll b) {
+        a = find(a); b = find(b);
+        if (a == b) return;
+        if (size(a) < size(b)) swap(a, b);
+        p[b] = a, sz[a] += sz[b];
+    }
+
+    ll size(ll num){ return sz[find(num)]; }
+    bool same(ll a, ll b) { return find(a) == find(b); }
+    void clear(){ p.assign(n + 1, -1); sz.assign(n + 1, 1); }
+}; 
+
+// WEIGHTED UNION FIND
+class _wuf { 
+public:
+    vector <ll> p, d, dis; ll n; 
+    _wuf(){}
+    _wuf(ll n) { 
+        this->n = n; dis.resize(n + 1);
+        p.resize(n + 1, -1); d.resize(n + 1); 
+    }
+
+    pll find(ll num) { 
+        if (p[num] == -1) return { num, 0ll };
+        auto [pa, diff] = find(p[num]);
+        p[num] = pa; d[num] += diff;
+        return { p[num], d[num] };
+    }
+
+    void merge(ll a, ll b, ll c) {
+        auto [pa, da] = find(a);
+        auto [pb, db] = find(b);
+        if (pa == pb) { dis[pb] |= (db - da - c != 0); return; }
+        p[pa] = pb; dis[pb] |= dis[pa];
+        d[pa] = db - da - c; 
+    }
+
+    pll diff(ll a, ll b){
+        auto [pa, da] = find(a);
+        auto [pb, db] = find(b);
+        if(pa == pb && !dis[pa]) return { 1, db - da };
+        else if(pa == pb && dis[pa]) return { 0, -1 };
+        else return { 0, 0 };
+    }
+
+    bool same(ll a, ll b) { return find(a).x == find(b).x; }
+};
+
+class _ufset{ 
+public:
+    ll n; vector <ll> p, si, l, r;
+    _ufset(){}
+    _ufset(ll n) { 
+        this->n = n;
+        p.resize(n + 1, -1); si.resize(n + 1, 1);
+        l.resize(n + 1); r.resize(n + 1);
+        iota(all(l), 0); iota(all(r), 0);
+    }
+
+    ll find(ll num) { 
+        if (p[num] == -1) return num;
+        return p[num] = find(p[num]);
+    }
+
+    void merge(ll a, ll b) {
+        a = find(a); b = find(b);
+        if (a == b) return;
+        if (size(a) < size(b)) swap(a, b);
+        p[b] = a, si[a] += si[b];
+        l[a] = min(l[a], l[b]);
+        r[a] = max(r[a], r[b]);
+    }
+
+    ll size(ll num){ return si[find(num)]; }
+    bool same(ll a, ll b) { return find(a) == find(b); }
+    ll prv(ll num){ return find(l[find(num)] - 1); }
+    ll nxt(ll num){ return find(r[find(num)] + 1); }
+};
