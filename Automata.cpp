98519104b42ec48a22b480751b03efd999e70fb5
kss418 <jis1595@naver.com>
2026-01-07 16:12:05 +0900

    아호코라식 수정

--- Automata.cpp
+++ Automata.cpp
@@ -249,8 +249,8 @@ public:
         cnt.reserve(max_node); end.reserve(max_node); dict.reserve(max_node);
     }
 
-    const vector<int>& depth() const{ return dep; } // O(1)
-    const vector<bool>& terminal() const{ return end; } // O(1)
+    const vector<int>& depth() const{ return dep; }
+    const vector<bool>& terminal() const{ return end; }
     int size() const{ return (int)adj.size(); } // O(1)
     void insert(const string& s){ insert(tf(s)); } // O(|s|)
     void insert(const vector <int>& v){ // O(|v|)
@@ -293,12 +293,8 @@ public:
 
     ll count(const string& s) const{ return count(tf(s)); } // O(|s|)
     ll count(const vector <int>& v) const{ // O(|v|)
-        assert(built); int cur = 0; ll ret = 0;
-        for(auto& c : v){
-            assert(0 <= c && c < M);
-            cur = go(cur, c);
-            ret += cnt[cur];
-        }
+        assert(built); ll ret = 0;
+        it_state(v, [&](int st){ ret += cnt[st]; });
         return ret;
     }
 
@@ -307,29 +303,31 @@ public:
         return adj[state][c];
     }
 
-    vector<int> max_match_len(const string& s) const{ return max_match_len(tf(s)); } // O(|s|)
-    vector<int> max_match_len(const vector<int>& v) const{ // O(|v|)
-        assert(built); vector<int> ret(v.size(), 0); int cur = 0;
-        for(int i = 0; i < (int)v.size(); i++){
-            int c = v[i]; assert(0 <= c && c < M); cur = go(cur, c);
-            ret[i] = end[cur] ? dep[cur] : (dict[cur] ? dep[dict[cur]] : 0);
+    template <class F> 
+    void it_state(const string& s, const F& f) const{ it_state(tf(s), f); } // O(|s|)
+    template <class F> 
+    void it_state(const vector <int>& v, const F& f) const{ // O(|v|)
+        assert(built); int cur = 0;
+        for(auto& c : v){
+            assert(0 <= c && c < M);
+            cur = go(cur, c), f(cur);
         }
-        return ret;
     }
 
-    vector<vector<int>> match_lens(const string& s, int k = 0x3f3f3f3f) const{ return match_lens(tf(s), k); } // O(|s| + matches)
-    vector<vector<int>> match_lens(const vector<int>& v, int k = 0x3f3f3f3f) const{ // O(|v| + matches)
-        assert(built); if(k <= 0) return vector<vector<int>>(v.size());
-        vector<vector<int>> ret(v.size()); int cur = 0;
-        for(int i = 0; i < (int)v.size(); i++){
-            int c = v[i]; assert(0 <= c && c < M); cur = go(cur, c);
-            if(end[cur]){
-                ret[i].push_back(dep[cur]);
-                if((int)ret[i].size() == k) continue;
-            }
-            for(int x = dict[cur];x > 0 && (int)ret[i].size() < k;x = dict[x]) ret[i].push_back(dep[x]);
+    int max_match_len(int state) const{ // O(1)
+        assert(built);
+        return end[state] ? dep[state] : (dict[state] ? dep[dict[state]] : 0);
+    }
+
+    template <class F>
+    void it_match_lens(int state, const F& f) const{ it_match_lens(state, 0x3f3f3f3f, f); } // O(min(k, matches))
+    template <class F>
+    void it_match_lens(int state, int k, const F& f) const{ // O(min(k, matches))
+        assert(built); if(k <= 0) return; int cnt = 0;
+        if(end[state]){
+            f(dep[state]); if(++cnt >= k) return;
         }
-        return ret;
+        for(int x = dict[state];x > 0 && cnt < k;x = dict[x]) f(dep[x]), ++cnt;
     }
 };
 
