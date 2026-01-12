619b6d43f445395cbd9e76417646d437679b88c4
kss418 <jis1595@naver.com>
2026-01-13 01:20:20 +0900

    2sat 코드 수정

--- Graph.cpp
+++ Graph.cpp
@@ -269,9 +269,9 @@ private:
         for(auto& nxt : rev[cur]) if(comp[nxt] == -1) dfs2(nxt, cid);
     }
 public:
-    _scc(int n = 0, int base = 1){ clear(n, base); } // O(n)
-    void clear(int n, int base = 1){ // O(n)
-        this->n = n; this->base = base;
+    _scc(int max_index = 0, int base = 1){ clear(n, base); } // O(n)
+    void clear(int max_index, int base = 1){ // O(n)
+        this->n = max_index; this->base = base;
         cc = 0; adj.assign(n + 1, {}); rev.assign(n + 1, {});
         comp.assign(n + 1, -1); group.clear(); ord.clear();
         v.assign(n + 1, false); ord.reserve(n + 1);
@@ -354,129 +354,45 @@ public:
 };
 
 //2SAT
-class _2sat { // 1-based index
+class _2sat{ // selcet base
+private:
+    int n = 0; bool base = 1;
 public:
-    ll n, dcnt = 0, scnt = 0;
-    vector <ll> d, sn, ind; // dfsn, scc번호, ind
-    vector<vector<ll>> adj, scc; // 간선, scc
-    vector <bool> fin;
-    stack <ll> st;
-    vector <set <ll>> sccset; // 위상정렬 간선
-
-    _2sat(ll n) {
-        this->n = n;
-        d.resize(2 * n + 1); fin.resize(2 * n + 1);
-        adj.resize(2 * n + 1); sn.resize(2 * n + 1);
+    _scc scc; 
+    _2sat(int max_index = 0){ clear(max_index); } // O(n)
+    void clear(int max_index, bool base = 1){ // O(n)
+        this->n = max_index; this->base = base;
+        scc.clear(2 * n + base - 1, base);
     }
 
-    ll no(ll cur) {
-        if (cur > n) return cur - n;
-        return cur + n;
-    }
+    int pos(int x) const{ return x; } // O(1)
+    int neg(int x) const{ return x + n; } // O(1)
+    int op(int x) const{ return x < n + base ? x + n : x - n; } // O(1)
 
-    void add(ll st, ll en) {
-        adj[no(st)].push_back(en);
-        adj[no(en)].push_back(st);
-    }
+    void add(int a, int b){ scc.addsol(a, b); } // O(1)
+    void add_or(int a, int b){ add(op(a), b); add(op(b), a); } // O(1)
+    void add_true(int x){ add(op(x), x); } // O(1)
+    void add_false(int x){ add_true(op(x)); } // O(1)
+    void add_eq(int a, int b){ add(a, b); add(b, a); } // O(1)
+    void add_xor(int a, int b){ add_or(a, b); add_or(op(a), op(b)); } // O(1)
 
-    ll dfs(ll cur) {
-        d[cur] = ++dcnt;
-        st.push(cur);
-
-        ll mn = d[cur];
-        for (auto& nxt : adj[cur]) {
-            if (!d[nxt]) mn = min(mn, dfs(nxt));
-            else if (!fin[nxt]) mn = min(mn, d[nxt]);
+    bool solve(){ // O(n + m)
+        scc.build(); 
+        for(int i = base;i < base + n;i++){
+            if(scc.same(pos(i), neg(i))) return false;
         }
-
-        if (mn == d[cur]) {
-            vector <ll> curscc;
-            while (1) {
-                ll t = st.top(); st.pop();
-                curscc.push_back(t);
-                fin[t] = 1; sn[t] = scnt;
-                if (t == cur) break;
-            }
-
-            sort(curscc.begin(), curscc.end());
-            scc.push_back(curscc); scnt++;
-        }
-
-        return mn;
+        return true;
     }
 
-    void init() {
-        for (int i = 1; i <= 2 * n; i++) {
-            if (d[i]) continue;
-            dfs(i);
+    template<class F>
+    void it(const F& f) const{ // O(n)
+        for(int i = base; i < base + 2 * n; i++){
+            int x = (scc.component(i) > scc.component(op(i))) ? 1 : 0;
+            f(i, x);
         }
-    }
-
-    vector <vector <ll>> sccret() { // scc 반환
-        return scc;
-    }
-
-    ll num(ll a) { // scc 번호 반환
-        return sn[a];
-    }
-
-    vector <ll> top() { //위상정렬
-        deque <ll> q; vector <ll> ts;
-        sccset.resize(2 * n + 1); ind.resize(2 * n + 1);
-
-        for (auto& i : scc) {
-            for (auto& cur : i) {
-                for (auto nxt : adj[cur]) {
-                    if (num(cur) == num(nxt)) continue;
-                    if (sccset[num(cur)].count(num(nxt))) continue;
-                    sccset[num(cur)].insert(num(nxt));
-                    ind[num(nxt)]++;
-                }
-            }
-        }
-
-        for (int i = 0; i < scnt; i++) {
-            if (ind[i]) continue;
-            q.push_back(i);
-        }
-
-        while (!q.empty()) {
-            ll cur = q.front(); q.pop_front();
-            ts.push_back(cur);
-
-            for (auto& nxt : sccset[cur]) {
-
-                if (!--ind[nxt]) q.push_back(nxt);
-            }
-        }
-
-        return ts;
-    }
-
-    ll ret() { // 2sat 여부 반환
-        ll flag = 1;
-        for (int i = 1; i <= n; i++) {
-            if (num(i) != num(no(i))) continue;
-            flag = 0; break;
-        }
-
-        return flag;
-    }
-
-    vector <ll> val() { // 참 일때 값 반환
-        vector <ll> ret;
-        ret.resize(2 * n + 1, -1);
-        vector <ll> track = top();
-        for (auto& i : track) {
-            for (auto& cur : scc[i]) {
-                if (ret[cur] != -1) continue;
-                ret[cur] = 0; ret[no(cur)] = 1;
-            }
-        }
-
-        return ret;
     }
 };
+
 
 //이분 매칭
 class _bm { // 1-based index
