77547684bab8c7b50b0a96ada9572f102e50c95b
kss418 <jis1595@naver.com>
2026-01-15 01:06:01 +0900

    line intersection 코드 수정

--- Geometry.cpp
+++ Geometry.cpp
@@ -30,30 +30,26 @@ public:
 //LINE INTERSECTION
 class _li{
 public:
-    _li() {}
-
-    ll ccw(pll v1, pll v2, pll v3){
-        ll num = (v2.x - v1.x) * (v3.y - v1.y);
-        num -= (v3.x - v1.x) * (v2.y - v1.y);
-
-        if(num > 0) return 1; 
-        else if(num < 0) return -1; 
+    static int ccw(const pll& v1, const pll& v2, const pll& v3){ // O(1)
+        i128 x1 = (i128)v2.x - v1.x, y1 = (i128)v2.y - v1.y;
+        i128 x2 = (i128)v3.x - v1.x, y2 = (i128)v3.y - v1.y;
+        i128 cross = x1 * y2 - x2 * y1;
+        if(cross > 0) return 1; 
+        else if(cross < 0) return -1; 
         return 0;
     }
 
-    bool ret(pair<pll, pll> a, pair<pll, pll> b) {
-        return ret(a.x, a.y, b.x, b.y);
-    }
-    bool ret(pll av1, pll av2, pll bv1, pll bv2){
-        ll ac = ccw(av1, av2, bv1) * ccw(av1, av2, bv2);
-        ll bc = ccw(bv1, bv2, av1) * ccw(bv1, bv2, av2);
-
+    static bool intersect(pair<pll, pll> a, pair<pll, pll> b){ // O(1)
+        return intersect(a.x, a.y, b.x, b.y); 
+    } 
+    static bool intersect(pll av1, pll av2, pll bv1, pll bv2){ // O(1)
+        int ac = ccw(av1, av2, bv1) * ccw(av1, av2, bv2);
+        int bc = ccw(bv1, bv2, av1) * ccw(bv1, bv2, av2);
         if(!ac && !bc){
             if(av1 > av2) swap(av1, av2);
             if(bv1 > bv2) swap(bv1, bv2);
             return bv1 <= av2 && av1 <= bv2;
         }
-
         return ac <= 0 && bc <= 0;
     }
 };
